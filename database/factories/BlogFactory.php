b737845a15999aebcc5e80a0d0715231e7d101c8
BreinerSan <breiner.sv@gmail.com>
2025-07-19 19:08:56 -0500

    LLenando de datos blog

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -17,8 +17,8 @@ class BlogFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->word(),
-            'color' => fake()->hexColor()
+            'title' => fake()->sentence(),
+            'content' => fake()->paragraph()
         ];
     }
 }
