f68670ae9b4e2a45eb862139c5c3dc01ab54e14c
BreinerSan <breiner.sv@gmail.com>
2025-08-02 20:36:30 -0500

    Eliminacion en cascada de preguntas

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -27,4 +27,32 @@ class Question extends Model
         return $this->morphMany(Comment::class, 'commentable');
     }
 
+    // Metodo propio de laravel que se ejecuta cuando se elimina un modelo
+    protected static function booted()
+    {
+        static::deleting(function ($question){
+            $question->hearts()->delete();
+
+            $question->comments()->get()->each(function ($comment) {
+                // Elimino los corazones
+                $comment->hearts()->delete();
+                // Elimina cada comentario asociado a la pregunta
+                $comment->delete();
+            });
+
+            // Elimina las respuestas asociadas a la pregunta
+            $question->answers()->get()->each(function ($answer) {
+                // Elimino los corazones de las respuestas
+                $answer->hearts()->delete();
+                // Elimino los comentarios de las respuestas
+                $answer->comments()->get()->each(function ($comment) {
+                    $comment->hearts()->delete();
+                    $comment->delete();
+                });
+                // Elimina cada respuesta asociada a la pregunta
+                // $answer->delete();
+            });
+        });
+    }
+
 }
